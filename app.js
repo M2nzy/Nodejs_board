da9b8ee4ddae6076bd05f90e32e749c73957fd08
M2nzy <yequrz@naver.com>
2018-10-02 03:02:37 +0900

    Routing test

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// router test
+var router = require('./routes/index')(app);
 
-app.listen(3000, function () {
-    console.log('Server On!');
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+/*
+mongoose.connect('mongodb://localhost/boards', { useNewUrlParser: true });
+var db = mongoose.connection;
+
+db.once("open", function () {
+    console.log("DB connected");
 });
+db.on("error", function (err) {
+    console.log("DB ERROR : ", err);
+})
+*/
+
+// set port and start server
+app.listen(3000, function(){
+    console.log('Server On');
+})
