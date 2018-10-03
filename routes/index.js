370efc09c50a9817147e37783da6d6efb8070d84
M2nzy <yequrz@naver.com>
2018-10-04 03:44:47 +0900

    Print post matching the parameters

--- routes/index.js
+++ routes/index.js
@@ -11,20 +11,31 @@ router.get('/', function (req, res) {
     connection.query('SELECT * FROM board ORDER BY date DESC', function (err, results) {
         if (err) console.error("query error : " + err);
         else {
-            res.render('index', {print : results});
+            res.render('index', { data: results });
+        }
+    });
+});
+
+// Print post (no=?)
+router.get('/board/:no', function (req, res) {
+    var no = req.params.no;
+    connection.query('SELECT title, id, date, hit, content from board WHERE no="' + no + '"', function (err, results) {
+        if (err) console.error("query error : " + err);
+        else {
+            res.render('board', { data: results });
         }
     });
 });
 
 // Create post page
-router.get('/create', function(req, res){
+router.get('/create', function (req, res) {
     res.render('create');
 });
 
 // Create process
-router.post('/create', function(req, res){
-    connection.query('INSERT INTO board (title, id, password, content, date) VALUES ("'+req.body.title+'", "'+req.body.id+'","'+req.body.password+'","'+req.body.content+'",now())', function(err, results){
-        if(err) console.error("query error : "+err);
+router.post('/create', function (req, res) {
+    connection.query('INSERT INTO board (title, id, password, content, date) VALUES ("' + req.body.title + '", "' + req.body.id + '","' + req.body.password + '","' + req.body.content + '",now())', function (err, results) {
+        if (err) console.error("query error : " + err);
         res.redirect('/');
     });
 });
