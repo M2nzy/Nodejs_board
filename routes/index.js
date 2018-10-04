df6460ffd4bb97714c7876d86c124f1453bcaa9c
M2nzy <yequrz@naver.com>
2018-10-04 20:55:18 +0900

    Delete post matching the parameters

--- routes/index.js
+++ routes/index.js
@@ -22,21 +22,46 @@ router.get('/board/:no', function (req, res) {
     connection.query('SELECT title, id, date, hit, content from board WHERE no="' + no + '"', function (err, results) {
         if (err) console.error("query error : " + err);
         else {
-            res.render('board', { data: results });
+            res.render('board', { no: no, title: results[0].title, id: results[0].id, date: results[0].date, hit: results[0].hit, content: results[0].content });
         }
     });
 });
 
 // Create post page
-router.get('/create', function (req, res) {
+router.get('/board', function (req, res) {
     res.render('create');
 });
 
 // Create process
-router.post('/create', function (req, res) {
+router.post('/board', function (req, res) {
     connection.query('INSERT INTO board (title, id, password, content, date) VALUES ("' + req.body.title + '", "' + req.body.id + '","' + req.body.password + '","' + req.body.content + '",now())', function (err, results) {
         if (err) console.error("query error : " + err);
         res.redirect('/');
+    });
+});
+
+// Enter ID and PASSWORD to delete
+router.get('/delete/:no', function(req, res){
+    var no = req.params.no;
+    res.render('delete', { no : no });
+});
+
+// Delete post page (no=?)
+router.post('/delete/:no', function (req, res) {
+    var no = req.params.no;
+    var ID = req.body.id;
+    var PASSWORD = req.body.password;
+    connection.query('SELECT id, password FROM board WHERE no="' + no + '"', function (err, results) {
+        // ID, PASSWORD correct => Delete
+        if (results[0].password === PASSWORD && results[0].id === ID) {
+            connection.query('DELETE FROM board WHERE no="' + no + '"', function (err, results) {
+                if (err) console.error('query error : ' + err);
+                res.redirect('/');
+            });
+        }
+        else {
+            res.redirect('/');
+        }
     });
 });
 
